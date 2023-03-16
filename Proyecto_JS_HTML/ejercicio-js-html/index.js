2410fdca4502a2ae32bf689948d3fa8a0a75d7e4
Javier Lavandier Tejada <javierlavandier@gmail.com>
2023-03-16 13:24:48 -0400

    Ejercicio sesion 14

---
+++ Proyecto_JS_HTML/ejercicio-js-html/index.js
@@ -0,0 +1,11 @@
+const boton = document.querySelector("#btn")
+
+boton.addEventListener("click", () => {
+    alert("click en el boton")
+})
+
+const botonJquery = document.querySelector("#jquery")
+
+botonJquery.addEventListener("click", () => {
+    console.log("Hola, estoy utilizando jQuery");
+})
